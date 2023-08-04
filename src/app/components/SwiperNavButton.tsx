871bd438c3ac074d5d9536a4bcb026b26f9c57dd
agustino42 <lopezagustin26@gmail.com>
2023-08-04 17:27:18 -0400

    dos y tres

--- src/app/components/SwiperNavButton.tsx
+++ src/app/components/SwiperNavButton.tsx
@@ -10,28 +10,26 @@ export const SwiperNavButton = () => {
     return (
     <Box m="1.5rem">
      <IconButton 
-     rounded="lg"
-     icon={<FaChevronLeft />}
-     borderColor="brand.primary"
-     borderWidth="1px"
-     color="brand.primaryDark"
-     bgColor="white"
-     arial-label="Prev"
-     onClick={() => swiper.slidePrev()}
-     mx="1"
-     />
+          rounded="lg"
+          icon={<FaChevronLeft />}
+          borderColor="brand.primary"
+          borderWidth="1px"
+          color="brand.primaryDark"
+          bgColor="white"
+          arial-label="Prev"
+          onClick={() => swiper.slidePrev()}
+          mx="1" aria-label={''}     />
 
 <IconButton 
-     rounded="lg"
-     icon={<FaChevronRight />}
-     borderColor="brand.primary"
-     borderWidth="1px"
-     color="brand.primaryDark"
-     bgColor="white"
-     arial-label="Next"
-     onClick={() => swiper.slideNext()}
-     mx="1"
-     />
+          rounded="lg"
+          icon={<FaChevronRight />}
+          borderColor="brand.primary"
+          borderWidth="1px"
+          color="brand.primaryDark"
+          bgColor="white"
+          arial-label="Next"
+          onClick={() => swiper.slideNext()}
+          mx="1" aria-label={''}     />
   
     </Box>
   );
